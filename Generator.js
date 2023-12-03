bd75a55ea5915601a0087af93ed3801f3e54f640
İbrahim Efe <93825421+Tennoxic@users.noreply.github.com>
2023-12-03 15:17:24 +0300

    Main Files

---
+++ Generator.js
@@ -0,0 +1,21 @@
+var myDiv=document.getElementsByTagName("div");
+
+for(let i=0;i<3;i++)
+{
+    for(let a=0;a<5;a++)
+    {
+        var myRandom=Math.floor(Math.random()*100)+1;
+
+        let newImage=document.createElement("img");
+
+        myDiv[0].appendChild(newImage);
+
+        newImage.setAttribute('id',`${i} ${a}`);
+
+        newImage.setAttribute('alt','Missing!')
+
+        newImage.setAttribute('style','margin:auto;width:330px; margin-bottom:20px;');
+
+        newImage.setAttribute('src',`https://source.unsplash.com/random/300x300?random=${myRandom}`);
+    }
+}
